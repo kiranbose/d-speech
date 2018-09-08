4b896d0e3af5a5d5ca73d7e4c339aaa9e96096b9
shbhattacharjee@deloitte.com <shbhattacharjee@deloitte.com>
2018-09-08 23:25:10 +0530

    Recorder

--- src/components/voicesPage/voicesPage.jsx
+++ src/components/voicesPage/voicesPage.jsx
@@ -1,45 +1,47 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { ReactMic } from 'react-mic';
-import { recordActions, recordingConstants } from '../../_actions';
-import './voices.scss'
+import React from "react";
+import { connect } from "react-redux";
+import { ReactMic } from "react-mic";
+import { recordActions, recordingConstants } from "../../_actions";
+import "./voices.scss";
+import RecordingCard from "../common/recordingCard/RecordingCard";
 
 const defaultProps = {
-    loop: false,
-    downloadable: true,
-    className: '',
-    style: {},
-    filename: 'output.wav',
-    playLabel: '🔊 Play',
-    playingLabel: '❚❚ Playing',
-    recordLabel: '● Record',
-    stopLabel: '● Stop',
-    removeLabel: '✖ Remove',
-    downloadLabel: '\ud83d\udcbe Save', // unicode floppy disk,
-    uploadForm: ''
-  };
+  loop: false,
+  downloadable: true,
+  className: "",
+  style: {},
+  filename: "output.wav",
+  playLabel: "🔊 Play",
+  playingLabel: "❚❚ Playing",
+  recordLabel: "● Record",
+  stopLabel: "● Stop",
+  removeLabel: "✖ Remove",
+  downloadLabel: "\ud83d\udcbe Save", // unicode floppy disk,
+  uploadForm: ""
+};
 
 class VoicesPage extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            test: ''
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      test: ""
+    };
+  }
 
-    render() {
-        return (
-            <div className="jumbotron">
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="jumbotron">
+        <RecordingCard />
+      </div>
+    );
+  }
 }
 
 function mapStateToProps(state) {
-    const { authentication } = state;
-    return {
-        authentication
-    };
+  const { authentication } = state;
+  return {
+    authentication
+  };
 }
 
 const connectedVoicesPage = connect(mapStateToProps)(VoicesPage);
